5505b29c0b61f08bbe6ca51389645a944801ec7e
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-12 22:14:06 +0530

    Next.js 14 Tutorial - 39 - URL Query Parameters

--- src/app/comments/route.js
+++ src/app/comments/route.js
@@ -1,7 +1,15 @@
 import { comments } from "./data";
 
-export async function GET() {
-  return new Response(JSON.stringify(comments), {
+export async function GET(request) {
+  const searchParams = request.nextUrl.searchParams;
+
+  const query = searchParams.get("query");
+
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return new Response(JSON.stringify(filteredComments), {
     headers: { "Content-Type": "application/json" },
   });
 }
