06465a0016978556ddfc832ab34c3bce3b0287de
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-13 11:13:25 +0530

    Next.js 14 Tutorial - 42 - Cookies in Route Handlers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,16 +1,24 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request) {
   const requestHeaders = new Headers(request.headers);
 
   const headersList = await headers();
 
+  await cookies().set("resultsPerPage", "20");
+
+  const theme = request.cookies.get("theme");
+
   console.log(requestHeaders.get("Authorization"));
   console.log(headersList.get("Authorization"));
+
+  console.log(theme);
+  console.log((await cookies()).get("resultsPerPage"));
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
