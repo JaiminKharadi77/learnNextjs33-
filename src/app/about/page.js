7a3214dad4556170fce41eb1fc978de804564423
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-20 21:05:41 +0530

    Next.js 14 Tutorial - 53 - Dynamic Rendering

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,19 @@
+import { cookies } from "next/headers";
+
 function AboutPage() {
+  const cookieStore = cookies();
+
+  const theme = cookieStore.get("theme");
+
+  console.log(theme);
+
   console.log("About server component");
 
-  return <div>This is About Page</div>;
+  return (
+    <div>
+      <h1>This is About Page {new Date().toLocaleTimeString()}</h1>
+    </div>
+  );
 }
 
 export default AboutPage;
