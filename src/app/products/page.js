291333e93ff1faad803bcaa981d2fb45412dd0b9
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-12-04 12:39:20 +0530

    Next.js 14 Tutorial - 70 - Time based Data Revalidation

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,12 +1,15 @@
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
 export default async function ProductsPage() {
-  const productsResponse = await fetch("http://localhost:3001/products");
+  const productsResponse = await fetch("http://localhost:3001/products", {
+    next: { revalidate: 10 }, // *NOTE 10 means 10seconds   it will return same data for 10s then it will store the new data
+  });
   const cookieStore = cookies();
   const theme = cookieStore.get("theme");
   console.log({ theme });
-  const detailsResponse = await fetch("http://localhost:3001/products/1");
-  const details = await detailsResponse.json();
+  // const detailsResponse = await fetch("http://localhost:3001/products/1");
+  // const details = await detailsResponse.json();
   const products = await productsResponse.json();
   return (
     <ul className="space-y-4 p-4">
@@ -18,7 +21,7 @@ export default async function ProductsPage() {
           <h2 className="text-xl font-semibold">{product.title}</h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
-          <p>{details.title}</p>
+          {/* <p>{details.title}</p> */}
         </li>
       ))}
     </ul>
